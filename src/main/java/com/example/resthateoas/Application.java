c8ab0c9ec8d4a2efd0f468283d84dcb6f856b614
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-13 22:29:52 +0900

    feat. 예제

---
+++ src/main/java/com/example/resthateoas/Application.java
@@ -0,0 +1,18 @@
+package com.example.resthateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ * User: clean_brain
+ * Date: 2023-11-13
+ * Comments: 
+ * </pre>
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
