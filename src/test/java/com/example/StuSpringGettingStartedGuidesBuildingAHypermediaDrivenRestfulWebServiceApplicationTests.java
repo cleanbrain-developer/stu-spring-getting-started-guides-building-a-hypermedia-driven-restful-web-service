887a73dda3eb0053592b61f307759c61d3a0858b
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-13 22:18:54 +0900

    first commit

---
+++ src/test/java/com/example/StuSpringGettingStartedGuidesBuildingAHypermediaDrivenRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuSpringGettingStartedGuidesBuildingAHypermediaDrivenRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
